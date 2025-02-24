7b43750b3c4e25db6a8d3d05db5283323da445e9
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-02-24 11:40:19 -0300

    trabalhando na tela inicial

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.titleBox}>
+      <Text style={ styles.tittleText}>Silvinha!</Text>
       <StatusBar style="auto" />
+    </View>
+
+    <StatusBar style= "auto" />
     </View>
   );
 }
@@ -13,8 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
+   paddingTop: 50
   },
 });
