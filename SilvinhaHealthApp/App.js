8428b82d37bf8e9072afb5d6d5802d619b258d50
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-02-27 12:26:44 -0300

    estilização

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,23 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.titleBox}>
-      <Text style={ styles.tittleText}>Silvinha!</Text>
+      <Text style={ styles.tittleText}>Silvinha health App</Text>
       <StatusBar style="auto" />
     </View>
-
-    <StatusBar style= "auto" />
-    </View>
+    
+    <StatusBar style= "light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-   paddingTop: 50
+    backgroundColor: '#edf2f4',
+   
   },
+  titleBox: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: 120,
+    backgroundColor: '#ef233c',
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
+  },
+  tittleText: {
+    color: '#edf2f4',
+    frontSize: 28,
+    frontWeight: 'bold',
+    marginBottom: 20,
+
+  }
+
 });
